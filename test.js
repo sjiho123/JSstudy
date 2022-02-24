ea424c523e0b97c4e058d00d4303e1a83c3feec4
서지호 <77095123+sjiho123@users.noreply.github.com>
2022-02-24 18:28:50 +0900

    오브젝트 데이터타입 md파일 작성

--- test.js
+++ test.js
@@ -1,20 +1,25 @@
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250
-};
+function slow(x) {
+    // CPU 집약적인 작업이 여기에 올 수 있습니다.
+    console.log(`slow(${x})을/를 호출함`);
+    return x;
+  }
+  
+  function cachingDecorator(func) {
+    let cache = new Map();
+  
+    return function(x) {
+      if (cache.has(x)) {    // cache에 해당 키가 있으면
+        return cache.get(x); // 대응하는 값을 cache에서 읽어옵니다.
+      }
+      console.log(typeof(func(x)))
+  
+      let result = func(x);  // 그렇지 않은 경우엔 func를 호출하고,
+      
+      cache.set(x, result);  // 그 결과를 캐싱(저장)합니다.
+      return result;
+    };
+  }
+  
+  slow = cachingDecorator(slow);
 
-function topSalary(salaries) {
-    let topsalary = 0
-    let topsalaryman = []
-    for (const [aa, bb] of Object.entries(salaries)) {
-
-        if (bb > topsalary) {
-            topsalary = bb
-            topsalaryman = aa
-        }
-    }
-    return console.log(topsalaryman);
-}
-
-topSalary(salaries)
+  slow(1)
